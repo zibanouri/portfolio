a88a29a2ee09851cbfc233c40a73178ee590dbee
Ziba Nouri <zibanouri13621115@gmail.com>
2025-08-09 20:53:38 +0330

    otomate topic badge

--- src/components/Project.Card.tsx
+++ src/components/Project.Card.tsx
@@ -1,13 +1,8 @@
-interface project {
-    imageURL: string;
-    title: string;
-    description: string;
-    topics: string[];
-    liveURL: string;
-    githubURL: string;
-}
+
 
 const ProjectCard = () => {
+    const topics = ["FrontEnd", "React", "Tailwind", "TypeScript", "Package"]
+
     return (
         <div className="max-w-80 mx-auto">
             <div className="bg-white/5 backdrop-blur-sm rounded-2xl overflow-hidden border border-gray-400/20 hover:border-gray-400/80 transition-all duration-300 group flex flex-col h-full">
@@ -26,21 +21,14 @@ const ProjectCard = () => {
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus quo nisi ipsum.
                     </p>
                     <div className="flex flex-wrap gap-2 mb-6">
-                        <span className="px-3 py-1 bg-slate-200 border border-slate-400 text-xs rounded-full font-medium">
-                            FrontEnd
-                        </span>
-                        <span className="px-3 py-1 bg-slate-200 border border-slate-400 text-xs rounded-full font-medium">
-                            React
-                        </span>
-                        <span className="px-3 py-1 bg-slate-200 border border-slate-400 text-xs rounded-full font-medium">
-                            Tailwind
-                        </span>
-                        <span className="px-3 py-1 bg-slate-200 border border-slate-400 text-xs rounded-full font-medium">
-                            Typescript
-                        </span>
-                        <span className="px-3 py-1 bg-slate-200 border border-slate-400 text-xs rounded-full font-medium">
-                            Package
-                        </span>
+                        {
+                            topics.map((topic) =>
+                                <span className="px-3 py-1 bg-slate-200 border border-slate-400 text-xs rounded-full font-medium">
+                                    {topic}
+                                </span>
+                            )
+                        }
+
                     </div>
                 </div>
             </div>
