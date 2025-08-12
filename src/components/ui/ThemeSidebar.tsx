e93651aef159d2cc04cb657ee12557bd396d5cba
Ziba Nouri <zibanouri13621115@gmail.com>
2025-08-12 19:57:59 +0330

    add theme toggle sidebar

--- src/components/ui/ThemeSidebar.tsx
+++ src/components/ui/ThemeSidebar.tsx
@@ -1,6 +1,8 @@
 const ThemeSidebar = () => {
     return(
-        <div>
+        <div className="fixed top-1/2 -translate-y-1/2 z-40 transition-all duration-300 right-0"
+        style={{transform : "transition-y-(-50%)"}}
+        >
             Theme Sidebar
         </div>
     )
