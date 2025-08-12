4b660f9053d6e00d80e1879f188c2ea0c9f3266f
Ziba Nouri <zibanouri13621115@gmail.com>
2025-08-12 20:42:46 +0330

    add Theme Mode in section ThemeSidebar

--- src/components/ui/ThemeSidebar.tsx
+++ src/components/ui/ThemeSidebar.tsx
@@ -1,9 +1,22 @@
+import { Sun, Moon } from "lucide-react";
+
 const ThemeSidebar = () => {
-    return(
+    return (
         <div className="fixed top-1/2 -translate-y-1/2 z-40 transition-all duration-300 right-0"
-        style={{transform : "transition-y-(-50%)"}}
+            style={{ transform: 'translateY(-50%)' }}
         >
-            Theme Sidebar
+            <div className="bg-white/95 backdrop-blur-md rounded-2xl shadow-2xl border border-blue-400/30 p-4 md:p-6 w-80 max-h-[90vh] overflow-y-auto">
+                <div className="flex text-center justify-between">
+                    <span className="text-sm font-medium text-slate-800 ">
+                        Theme Mode 
+                    </span>
+                    <button className="p-2 rounded-lg bg-slate-100 hover:bg-slate-200 transition-colors duration-300">
+                        <Moon className="w-4 h-4 text-slate-700" />
+                    </button>
+                </div>
+
+            </div>
+
         </div>
     )
 
