cbe8559eae8b514b9934e138b97392379de27c92
Ziba Nouri <zibanouri13621115@gmail.com>
2025-08-16 19:46:05 +0330

    apply dark mode to sidebar

--- src/components/ui/ThemeSidebar.tsx
+++ src/components/ui/ThemeSidebar.tsx
@@ -1,4 +1,4 @@
-import { SunMoon, Moon, Palette, Languages, ChevronLeft, ChevronRight } from 'lucide-react';
+import { SunMoon, Moon,Sun, Palette, Languages, ChevronLeft, ChevronRight } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
 
@@ -50,18 +50,26 @@ const ThemeSidebar = () => {
 
             <div  className={`fixed top-1/2 -translate-y-1/2 z-40 transition-all duration-300
                 ${isOpen ? 'right-0' : '-right-80'}`}>
-                <div className="bg-white/95 backdrop-blur-md rounded-2xl shadow-2xl border border-blue-400/30 p-4 md:p-6 w-80 max-h-[90vh] overflow-y-auto" data-sidebare>
+                <div className="bg-white/95 dark:bg-slate-800/95 backdrop-blur-md rounded-2xl shadow-2xl border border-blue-400/30 p-4 md:p-6 w-80 max-h-[90vh] overflow-y-auto" data-sidebare>
                     <div className="flex items-center justify-between mb-3">
-                        <span className="text-sm font-medium flex items-center text-slate-700">
+                        <span className="text-sm font-medium flex items-center text-slate-700 dark:text-slate-200">
                             <SunMoon className="w-4 h-4 mr-2" />
                             Theme Mode
                         </span>
-                        <button className="p-2 rounded-lg bg-slate-100 hover:bg-slate-200 transition-colors duration-300 transform transform-gpu hover:scale-[1.1] cursor-pointer">
-                            <Moon className="w-4 h-4 text-slate-600" />
+                        <button className="p-2 rounded-lg bg-slate-100 hover:bg-slate-200 dark:bg-slate-900 dark:hover:bg-slate-800 transition-colors duration-300 transform transform-gpu hover:scale-[1.1] cursor-pointer"
+                              onClick={() =>
+                                setMode(mode === 'light' ? 'dark' : 'light')
+                            }
+                        >
+                            {mode === 'light' ? (
+                                <Moon className="w-4 h-4 text-slate-600" />
+                            ) : (
+                                <Sun className="w-4 h-4 text-slate-300" />
+                            )}
                         </button>
                     </div>
                     <div>
-                        <h3 className="text-sm font-medium mb-3 flex items-center text-slate-700">
+                        <h3 className="text-sm font-medium mb-3 flex items-center text-slate-700 dark:text-slate-200">
                             <Palette className="w-4 h-4 mr-2" />
                             Color Themes
                         </h3>
@@ -69,7 +77,7 @@ const ThemeSidebar = () => {
                             {themes.map((theme, index) => (
                                 <button
                                     key={index}
-                                    className="p-2 md:p-3 rounded-lg border-2 transition-all duration-300 transform transform-gpu hover:scale-[1.1] text-slate-700 border-transparent hover:border-slate-300 cursor-pointer"
+                                    className="p-2 md:p-3 rounded-lg border-2 transition-all duration-300 transform transform-gpu hover:scale-[1.1] text-slate-700 dark:text-slate-200 border-transparent hover:border-slate-300 cursor-pointer"
                                 >
                                     <div
                                         className={`w-5 h-5 rounded-full md:w-6 md:h-6 ${theme.color} mx-auto mb-1`}>
@@ -81,7 +89,7 @@ const ThemeSidebar = () => {
                         </div>
                     </div>
                     <div>
-                        <h3 className="text-sm font-medium mb-3 flex items-center text-slate-700 mt-3">
+                        <h3 className="text-sm font-medium mb-3 flex items-center text-slate-700 mt-3 dark:text-slate-200">
                             <Languages className="w-4 h-4 mr-2" />
                             Languages
                         </h3>
@@ -89,7 +97,7 @@ const ThemeSidebar = () => {
                             {languages.map((lang, index) => (
                                 <button
                                     key={index}
-                                    className="w-full p-2 rounded-lg text-left transition-all duration-300 transform transform-gpu hover:scale-[1.1] text-slate-700 hover:bg-slate-100 cursor-pointer"
+                                    className="w-full p-2 rounded-lg text-left transition-all duration-300 transform transform-gpu hover:scale-[1.1] text-slate-700 hover:bg-slate-100 dark:text-slate-200 dark:hover:bg-slate-800 cursor-pointer"
                                 >
 
                                     <span className="text-sm">{lang.name}</span>
